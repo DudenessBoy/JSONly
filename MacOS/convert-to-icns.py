709bd68772efa64dafb575954ade485dd7745a31
Luke Moyer <DudenessBoy_software@disroot.org>
2025-04-12 19:45:07 -0400

    Fixed convert-to-icns.py

--- MacOS/convert-to-icns.py
+++ MacOS/convert-to-icns.py
@@ -1,11 +1,7 @@
 # Copyright (c) 2025 Luke Moyer
 # Licensed under the MIT License. See LICENSE file for details.
 
-import icnsutil
+from PIL import Image
 
-# Load the PNG file
-icon = icnsutil.IcnsFile()
-icon.add_media(file='../icon.png')
-
-# Write the ICNS file
-icon.write('JSONly/resources/icon.icns')
+icon = Image.open('../icon.png')
+icon.save('JSONly/resources/icon.icns', format='ICNS')
